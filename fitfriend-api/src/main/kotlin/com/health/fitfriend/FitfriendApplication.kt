c6a2f62ff1f4f685e5238f25da5a7e430451a720
Aditya Mishra <adityamishra2217@gmail.com>
2023-06-16 17:41:06 +0530

    Initial setup

---
+++ fitfriend-api/src/main/kotlin/com/health/fitfriend/FitfriendApplication.kt
@@ -0,0 +1,11 @@
+package com.health.fitfriend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FitfriendApplication
+
+fun main(args: Array<String>) {
+	runApplication<FitfriendApplication>(*args)
+}
