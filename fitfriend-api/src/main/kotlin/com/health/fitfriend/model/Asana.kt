8c135df588c6329ef3cc052f1ea75bebb803b6bf
Aditya Mishra <adityamishra2217@gmail.com>
2023-06-17 17:52:20 +0530

    Created model class for asanas

---
+++ fitfriend-api/src/main/kotlin/com/health/fitfriend/model/Asana.kt
@@ -0,0 +1,12 @@
+package com.health.fitfriend.model
+
+data class Asana(
+    val id: Int,
+    val name: String,
+    val introduction: String,
+    val benefits: String,
+    val directions: String,
+    val imageUrl: String,
+    val videoUrl: String,
+    val precautions: String,
+)
