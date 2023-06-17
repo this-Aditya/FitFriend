891b3e5010578a7ac8770d02aa7b0d6f48fcabee
Aditya Mishra <adityamishra2217@gmail.com>
2023-06-17 17:55:57 +0530

    Added service layer for asanas

---
+++ fitfriend-api/src/main/kotlin/com/health/fitfriend/service/AsanaService.kt
@@ -0,0 +1,14 @@
+package com.health.fitfriend.service
+
+import com.health.fitfriend.datasource.AsanaDataSource
+import com.health.fitfriend.model.Asana
+import org.springframework.stereotype.Service
+
+@Service
+class AsanaService(private val asanaDataSource: AsanaDataSource) {
+
+    fun getAsanas(): Collection<Asana> = asanaDataSource.retrieveAsanas()
+    fun getAsanaById(id: Int): Asana = asanaDataSource.retrieveAsanaById(id)
+    fun getAsanaByName(name: String): Asana = asanaDataSource.retrieveAsanaByName(name)
+
+}
