e3c81a4b4285ee3ad5100fac6b0d2ad94ac4f93b
Aditya Mishra <adityamishra2217@gmail.com>
2023-06-19 22:18:04 +0530

    Added service layer for pranayam

---
+++ fitfriend-api/src/main/kotlin/com/health/fitfriend/service/PranayamService.kt
@@ -0,0 +1,16 @@
+package com.health.fitfriend.service
+
+import com.health.fitfriend.datasource.PranayamDataSource
+import com.health.fitfriend.model.Pranayam
+import org.springframework.stereotype.Service
+
+
+@Service
+class PranayamService(private val pranayamDataSource: PranayamDataSource) {
+
+    fun getPranayams(): Collection<Pranayam> = pranayamDataSource.retrivePranayams()
+
+    fun getpranayamById(id: Int): Pranayam = pranayamDataSource.retrivePranayamById(id)
+
+    fun getpranayamByName(name: String) = pranayamDataSource.retrivePranayamByName(name)
+}
