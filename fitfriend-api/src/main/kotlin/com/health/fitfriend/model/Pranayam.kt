7b1fa301a19ae49cdfca18b9151adc44baf0f201
Aditya Mishra <adityamishra2217@gmail.com>
2023-06-19 15:49:50 +0530

    Added Pranayam data class

---
+++ fitfriend-api/src/main/kotlin/com/health/fitfriend/model/Pranayam.kt
@@ -0,0 +1,13 @@
+package com.health.fitfriend.model
+
+
+data class Pranayam(
+    val id: Int,
+    val name: Int,
+    val introduction: String,
+    val benefits: String,
+    val directions: String,
+    val precautions: String,
+    val imageUrl: String,
+    val videoUrl: String
+)
