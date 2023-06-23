65ce60da30857575ff9f5f0b0422ea65cf8cb9d3
Aditya Mishra <adityamishra2217@gmail.com>
2023-06-23 11:13:44 +0530

    Added data model for meditation

---
+++ fitfriend-api/src/main/kotlin/com/health/fitfriend/model/Meditation.kt
@@ -0,0 +1,11 @@
+package com.health.fitfriend.model
+
+data class Meditation(
+    val id: Int,
+    val name: String,
+    val benefits: String,
+    val instructions: String,
+    val precautions: String,
+    val imageUrl: String,
+    val videoUrl: String,
+)
