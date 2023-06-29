1863df1c7db0991296ce5c9777c6162ba4ac96b1
Aditya Mishra <adityamishra2217@gmail.com>
2023-06-29 22:42:12 +0530

    Added model classes for Asana, Pranayam and Meditation

---
+++ app/src/main/java/com/aditya/fitfriend_android/models/Asana.kt
@@ -0,0 +1,12 @@
+package com.aditya.fitfriend_android.models
+
+data class Asana(
+    val id: Int,
+    val name: String,
+    val introduction: String,
+    val benefits: String,
+    val directions: String,
+    val precautions: String,
+    val imageUrl: String,
+    val videoUrl: String
+)
---
+++ app/src/main/java/com/aditya/fitfriend_android/models/Meditation.kt
@@ -0,0 +1,12 @@
+package com.aditya.fitfriend_android.models
+
+data class Meditation(
+    val id: Int,
+    val name: String,
+    val introduction: String,
+    val benefits: String,
+    val instructions: String,
+    val precautions: String,
+    val imageUrl: String,
+    val videoUrl: String
+)
