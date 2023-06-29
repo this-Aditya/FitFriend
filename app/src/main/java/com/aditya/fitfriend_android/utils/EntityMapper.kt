14fe51f51f46b261a704e0be93548d5893e43b3a
Aditya Mishra <adityamishra2217@gmail.com>
2023-06-29 23:01:27 +0530

    Added Entity Mapper interface as util

--- app/src/main/java/com/aditya/fitfriend_android/utils/EntityMapper.kt
+++ app/src/main/java/com/aditya/fitfriend_android/utils/EntityMapper.kt
@@ -1,4 +1,10 @@
 package com.aditya.fitfriend_android.utils
 
-class EntityMapper {
+interface EntityMapper<Entity, DomainModel> {
+
+    fun mapFromEntity(entity: Entity): DomainModel
+    fun mapToEntity(domainModel: DomainModel): Entity
+    fun mapFromEntityList(entities: List<Entity>): List<DomainModel>
+    fun mapToEntityList(domainModels: List<DomainModel>): List<Entity>
+
 }
