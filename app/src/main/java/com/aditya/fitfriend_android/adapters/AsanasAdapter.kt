5098631e47ffad58018dbc5873fea597f900b6e9
Aditya Mishra <adityamishra2217@gmail.com>
2023-07-02 15:59:20 +0530

    Added asana adapter

---
+++ app/src/main/java/com/aditya/fitfriend_android/adapters/AsanasAdapter.kt
@@ -0,0 +1,28 @@
+package com.aditya.fitfriend_android.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.aditya.fitfriend_android.abstracts.AbstractyogasAdapter
+import com.aditya.fitfriend_android.databinding.ItemRowLayoutBinding
+import com.aditya.fitfriend_android.models.Asana
+import com.squareup.picasso.Picasso
+
+
+class AsanasAdapter(context: Context, itemClicked: (Asana) -> Unit) : AbstractyogasAdapter<Asana>(
+    context, itemClicked) {
+
+    override var yogas: List<Asana> = emptyList()
+
+    class AsanaViewholder(binding: ItemRowLayoutBinding) : AbstractViewHolder<Asana>(binding) {
+        override fun bindView(yoga: Asana) {
+            super.binding.tvRow.text = yoga.name
+            Picasso.get().load(yoga.imageUrl).into(binding.ivRowImage)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AbstractViewHolder<Asana> {
+        binding = ItemRowLayoutBinding.inflate(LayoutInflater.from(context), parent, false)
+        return AsanaViewholder(binding)
+    }
+}
