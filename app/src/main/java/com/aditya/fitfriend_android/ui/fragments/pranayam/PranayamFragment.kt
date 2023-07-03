b3760a7a848e87e547c91a4b8051758d4b050204
Aditya Mishra <adityamishra2217@gmail.com>
2023-07-03 17:50:08 +0530

    Added list and detail fragment for pranayam and some error handling while sign-up

--- app/src/main/java/com/aditya/fitfriend_android/ui/fragments/pranayam/PranayamFragment.kt
+++ app/src/main/java/com/aditya/fitfriend_android/ui/fragments/pranayam/PranayamFragment.kt
@@ -1,18 +1,184 @@
 package com.aditya.fitfriend_android.ui.fragments.pranayam
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
 import com.aditya.fitfriend_android.R
+import com.aditya.fitfriend_android.data.entities.PranayamCacheEntity
+import com.aditya.fitfriend_android.data.mappers.PranayamCacheMapper
+import com.aditya.fitfriend_android.databinding.FragmentPranayamBinding
+import com.aditya.fitfriend_android.models.Pranayam
+import com.aditya.fitfriend_android.utils.DataState
+import com.aditya.fitfriend_android.viewmodels.PranayamStateEvent
+import com.aditya.fitfriend_android.viewmodels.PranayamViewModel
+import com.squareup.picasso.Picasso
+import dagger.hilt.android.AndroidEntryPoint
 
+private const val TAG = "PranayamFragment"
+
+@AndroidEntryPoint
 class PranayamFragment : Fragment() {
+
+    private lateinit var binding: FragmentPranayamBinding
+    private lateinit var pranayam: Pranayam
+    private val viewModel: PranayamViewModel by viewModels()
+    private val args: PranayamFragmentArgs by navArgs()
+    private val pranayamCacheMapper = PranayamCacheMapper()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_pranayam, container, false)
+    ): View {
+        binding = FragmentPranayamBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val id = args.pranayamId
+        handleActionButton()
+        subscribeToObservers()
+        viewModel.setStateEvent(PranayamStateEvent.GetPranayamEvent(id))
+        handleClicks()
+        binding.fabYoga.visibility = View.GONE
+        binding.btnVideoPranayamD.visibility = View.GONE
+    }
+
+    private fun handleActionButton() {
+        if (args.isCachedPranayam) binding.fabYoga.setImageResource(R.drawable.delete_yoga)
+        else binding.fabYoga.setImageResource(R.drawable.add_yoga)
+    }
+
+    private fun handleClicks() {
+
+        binding.btnIntroExpandPranayamD.setOnClickListener {
+            if (binding.tvIntroDetailsPranayamD.visibility == View.GONE) {
+                binding.tvIntroDetailsPranayamD.visibility = View.VISIBLE
+                binding.btnIntroExpandPranayamD.setImageResource(R.drawable.arrow_up)
+            } else {
+                binding.tvIntroDetailsPranayamD.visibility = View.GONE
+                binding.btnIntroExpandPranayamD.setImageResource(R.drawable.arrow_down)
+            }
+        }
+        binding.btnDescrExpandPranayamD.setOnClickListener {
+            if (binding.tvDescrDetailsPranayamD.visibility == View.GONE) {
+                binding.tvDescrDetailsPranayamD.visibility = View.VISIBLE
+                binding.btnDescrExpandPranayamD.setImageResource(R.drawable.arrow_up)
+            } else {
+                binding.tvDescrDetailsPranayamD.visibility = View.GONE
+                binding.btnDescrExpandPranayamD.setImageResource(R.drawable.arrow_down)
+            }
+        }
+        binding.btnBenefitsExpandPranayamD.setOnClickListener {
+            if (binding.tvBenefitsDetailsPranayamD.visibility == View.GONE) {
+                binding.tvBenefitsDetailsPranayamD.visibility = View.VISIBLE
+                binding.btnBenefitsExpandPranayamD.setImageResource(R.drawable.arrow_up)
+            } else {
+                binding.tvBenefitsDetailsPranayamD.visibility = View.GONE
+                binding.btnBenefitsExpandPranayamD.setImageResource(R.drawable.arrow_down)
+            }
+        }
+        binding.btnPrecautionsExpandPranayamD.setOnClickListener {
+            if (binding.tvPrecautionsDetailPranayamD.visibility == View.GONE) {
+                binding.tvPrecautionsDetailPranayamD.visibility = View.VISIBLE
+                binding.btnPrecautionsExpandPranayamD.setImageResource(R.drawable.arrow_up)
+            } else {
+                binding.tvPrecautionsDetailPranayamD.visibility = View.GONE
+                binding.btnPrecautionsExpandPranayamD.setImageResource(R.drawable.arrow_down)
+            }
+        }
+        binding.btnVideoPranayamD.setOnClickListener {
+            val appIntent = Intent(Intent.ACTION_VIEW, Uri.parse(pranayam.videoUrl))
+            val webIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse(pranayam.videoUrl)
+            )
+            try {
+                startActivity(appIntent)
+            } catch (ex: ActivityNotFoundException) {
+                startActivity(webIntent)
+            }
+        }
+        binding.fabYoga.setOnClickListener {
+            val pranayam: PranayamCacheEntity = pranayamCacheMapper.mapToEntity(pranayam)
+            if (!args.isCachedPranayam) {
+                viewModel.setStateEvent(PranayamStateEvent.AddPranayamToCacheEvent(pranayam))
+                Toast.makeText(
+                    requireContext(),
+                    "Successfully saved ${pranayam.name}.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                viewModel.setStateEvent(PranayamStateEvent.DeleteCachedPranayam(pranayam))
+                Toast.makeText(requireContext(), "Deleted ${pranayam.name}.", Toast.LENGTH_SHORT)
+                    .show()
+                val action =
+                    PranayamFragmentDirections.actionPranayamFragmentToPranayamListFragment(true)
+                requireView().findNavController().navigate(action)
+            }
+        }
+    }
+
+    private fun subscribeToObservers() {
+        Log.i(TAG, "Subscribed to observers for Pranayam")
+        viewModel.pranayamDataState.observe(viewLifecycleOwner) { datastate ->
+            when (datastate) {
+                is DataState.Loading -> {
+                    Log.d(TAG, "Data is loading ")
+                    showProgressBar(true)
+                }
+
+                is DataState.Error -> {
+                    val exp = "Unable to resolve host \"fitfriend.onrender.com\": No address associated with hostname"
+                    if (datastate.ex.message.equals(exp)) {
+                        Toast.makeText(requireContext(), "No internet connection", Toast.LENGTH_SHORT).show()
+                        return@observe
+                    }
+                    showProgressBar(false)
+                    Toast.makeText(requireContext(), "${datastate.ex}", Toast.LENGTH_SHORT).show()
+                }
+
+                is DataState.Success -> {
+                    showProgressBar(false)
+                    pranayam = datastate.data
+                    Log.i(TAG, "Pranayam received -> ${pranayam.id} ${pranayam.name}")
+                    processData()
+                    binding.fabYoga.visibility = View.VISIBLE
+                    binding.btnVideoPranayamD.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    private fun processData() {
+        Picasso.get().load(pranayam.imageUrl).into(binding.ivPranayamD)
+        binding.tvPranayamD.text = pranayam.name
+        binding.tvIntroDetailsPranayamD.text = pranayam.introduction
+        binding.tvDescrDetailsPranayamD.text = pranayam.directions
+        binding.tvBenefitsDetailsPranayamD.text = pranayam.benefits
+        binding.tvPrecautionsDetailPranayamD.text = pranayam.precautions
+
+
+    }
+
+    private fun showProgressBar(show: Boolean) {
+        binding.progressBarpranayam.visibility = if (show) View.VISIBLE else View.GONE
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: PranayamFragment")
+    }
+
 }
--- app/src/main/java/com/aditya/fitfriend_android/ui/fragments/pranayam/PranayamListFragment.kt
+++ app/src/main/java/com/aditya/fitfriend_android/ui/fragments/pranayam/PranayamListFragment.kt
@@ -1,18 +1,161 @@
 package com.aditya.fitfriend_android.ui.fragments.pranayam
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.aditya.fitfriend_android.R
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.aditya.fitfriend_android.adapters.PranayamsAdapter
+import com.aditya.fitfriend_android.data.mappers.PranayamCacheMapper
+import com.aditya.fitfriend_android.databinding.FragmentPranayamListBinding
+import com.aditya.fitfriend_android.models.Pranayam
+import com.aditya.fitfriend_android.utils.DataState
+import com.aditya.fitfriend_android.viewmodels.PranayamStateEvent
+import com.aditya.fitfriend_android.viewmodels.PranayamViewModel
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+private const val TAG = "PranayamListFragment"
+
+@AndroidEntryPoint
 class PranayamListFragment : Fragment() {
+
+    private lateinit var binding: FragmentPranayamListBinding
+    private lateinit var adapter: PranayamsAdapter
+    private val viewModel: PranayamViewModel by viewModels()
+    private var snackbar: Snackbar? = null
+    private val args: PranayamListFragmentArgs by navArgs()
+    private val pranayamCacheMapper = PranayamCacheMapper()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (!args.isCachedPranayams) {
+            viewModel.setStateEvent(PranayamStateEvent.GetPranayamsEvent)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_pranayam_list, container, false)
+    ): View {
+        Log.i(TAG, "onCreateView: ")
+        binding = FragmentPranayamListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.i(TAG, "onViewCreated: ")
+        binding.progressBarPranayams.visibility = View.GONE
+        setUpAdapter()
+        setUpRecyclerView()
+        if (!args.isCachedPranayams) {
+            subscribeToRemoteObservers()
+        } else {
+            viewModel.setStateEvent(PranayamStateEvent.GetCachedPranayamsEvent)
+            subscribeToLocalObservers()
+        }
+    }
+
+    private fun setUpAdapter() {
+        val view = requireView()
+        adapter = PranayamsAdapter(requireContext()) { Pranayam ->
+            Log.i(TAG, "Navigating from list to detail fragment view")
+            Log.i(TAG, "Navigating to Pranayam -> Name: ${Pranayam.name} ID: ${Pranayam.id}")
+            val action =
+                PranayamListFragmentDirections.actionPranayamListFragmentToPranayamFragment(
+                    args.isCachedPranayams,
+                    Pranayam.id
+                )
+            view.findNavController().navigate(action)
+        }
+    }
+
+    private fun subscribeToRemoteObservers() {
+        viewModel.pranayamsDataState.observe(viewLifecycleOwner, Observer { datastate ->
+            when (datastate) {
+                is DataState.Success<List<Pranayam>> -> {
+                    showProgressBar(false)
+                    showSnackBar(false)
+                    adapter.setData(datastate.data)
+                    Log.i(TAG, "Data received successfully")
+                }
+
+                is DataState.Loading -> {
+                    showProgressBar(true)
+                    lifecycleScope.launch {
+                        delay(1500)
+                        if (binding.progressBarPranayams.visibility == View.VISIBLE) showSnackBar(
+                            true
+                        )
+                    }
+                    Log.i(TAG, "Data loading")
+                }
+
+                is DataState.Error -> {
+                    if (datastate.ex.message.equals("timeout")) {
+                        viewModel.setStateEvent(PranayamStateEvent.GetPranayamsEvent)
+                        return@Observer
+                    }
+                    val exp = "Unable to resolve host \"fitfriend.onrender.com\": No address associated with hostname"
+                    if (datastate.ex.message.equals(exp)) {
+                        Toast.makeText(requireContext(), "No internet connection", Toast.LENGTH_SHORT).show()
+                        return@Observer
+                    }
+                    showProgressBar(false)
+                    showSnackBar(false)
+                    Toast.makeText(requireContext(), "${datastate.ex.message}", Toast.LENGTH_SHORT)
+                        .show()
+                    Log.w(TAG, "Exception while receiving data: ${datastate.ex}")
+                }
+            }
+        })
+    }
+
+    private fun subscribeToLocalObservers() {
+        viewModel.cachedPranayamList.observe(viewLifecycleOwner) { cachedPranayams ->
+            val pranayams: List<Pranayam> = pranayamCacheMapper.mapFromEntityList(cachedPranayams)
+            adapter.setData(pranayams)
+        }
+    }
+
+    private fun showSnackBar(show: Boolean) {
+        if (show) {
+            Log.d(TAG, "Snackbar loading...")
+            snackbar = Snackbar.make(
+                requireView(),
+                "Please wait as it may take some time to load for the first time. We apologize for any inconvenience caused, as we are actively working on expanding our servers.",
+                Snackbar.LENGTH_INDEFINITE
+            )
+            snackbar?.show()
+        } else {
+            snackbar?.dismiss()
+            Log.d(TAG, "Dismissed Snackbar")
+        }
+    }
+
+    private fun showProgressBar(show: Boolean) {
+        Log.d(TAG, "ProgressBar Visibility: $show")
+        binding.progressBarPranayams.visibility = if (show) View.VISIBLE else View.GONE
+    }
+
+    private fun setUpRecyclerView() {
+        binding.rvPranayams.adapter = adapter
+        binding.rvPranayams.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        snackbar?.dismiss()
     }
 }
