d19e60b54a19d860fb55ed9a3e3df8587c3c4612
Aditya Mishra <adityamishra2217@gmail.com>
2023-07-03 23:06:18 +0530

    Added Utility classes for notification and permissions functionality and some documentation

--- app/src/main/java/com/aditya/fitfriend_android/utils/DataState.kt
+++ app/src/main/java/com/aditya/fitfriend_android/utils/DataState.kt
@@ -1,5 +1,12 @@
 package com.aditya.fitfriend_android.utils
 
+/**
+ * States of data receiving from network request with .
+ * Success -> Data is received successfully, data is present as a constructor argument.
+ * Error -> Some exception occurred while receiving data. cause is present as a constructor argument.
+ * Loading -> Data is loading currently.
+ */
+
 sealed class DataState<out R> {
 
     data class Success<out T>(val data: T): DataState<T>()
---
+++ app/src/main/java/com/aditya/fitfriend_android/utils/NotificationHandler.kt
@@ -0,0 +1,54 @@
+package com.aditya.fitfriend_android.utils
+
+import android.Manifest
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import com.aditya.fitfriend_android.R
+import com.aditya.fitfriend_android.ui.MainActivity
+
+class NotificationHandler {
+
+
+    fun showNotification(context: Context, channelId: String, title: String, message: String) {
+        createPendingIntent(context)
+        val notificationBuilder = NotificationCompat.Builder(context, channelId)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+        val notificationManager = NotificationManagerCompat.from(context)
+        if (ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+        }
+    }
+
+    companion object {
+
+        val pendingIntent: PendingIntent? = null
+
+        fun createPendingIntent(context: Context) {
+            if (pendingIntent != null) return
+            val intent = Intent(context, MainActivity::class.java)
+            PendingIntent.getActivity(
+                context,
+                NOTIFICATION_PI_CODE,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+        }
+
+        private const val NOTIFICATION_PI_CODE = 917906
+        const val NOTIFICATION_ID = 423894
+    }
+}
