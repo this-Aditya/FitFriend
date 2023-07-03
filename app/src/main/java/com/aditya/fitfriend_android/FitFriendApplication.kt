f63a405df76aa4b75469e99ec83ce212a611a0c1
Aditya Mishra <adityamishra2217@gmail.com>
2023-07-03 23:06:18 +0530

    Added Utility classes for notification and permissions functionality and some documentation

--- app/src/main/java/com/aditya/fitfriend_android/FitFriendApplication.kt
+++ app/src/main/java/com/aditya/fitfriend_android/FitFriendApplication.kt
@@ -1,8 +1,38 @@
 package com.aditya.fitfriend_android
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class FitFriendApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel =NotificationChannel(
+                CHANNEL_ID, "fit_friend_notification", NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val notificationManger = getSystemService(NotificationManager::class.java)
+            notificationManger.createNotificationChannel(channel)
+        }
+    }
+
+    companion object {
+        const val CHANNEL_ID = "872594"
+
+        fun createSleepPendingIntent() {
+
+        }
+
+        fun createActivityPendingIntent() {
+
+        }
+    }
 }
---
+++ app/src/main/java/com/aditya/fitfriend_android/utils/PermissionHandler.kt
@@ -0,0 +1,82 @@
+package com.aditya.fitfriend_android.utils
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.content.pm.PackageManager
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+private const val TAG = "PermissionHandler"
+
+/**
+ * Utility permission handler, for asking for the permission just create the instance of the class
+ * and call the `requestPermissions` Method.
+ */
+class PermissionHandler {
+
+    /**
+     * This mehod should be called in order to request for permissions.
+     * @param context context required for permissions (should be the context of an activity.)
+     * @param permissions list of permissions
+     * @param launcher launcher which is launching the permission dialogue
+     * @param message message to be displayed as a rationale when user once declines permission
+     */
+    fun requestPermissions(
+        context: Activity,
+        permissions: List<String>,
+        launcher: ActivityResultLauncher<Array<String>>,
+        message: String
+    ) {
+        val permissionsToRequest = mutableListOf<String>()
+
+        permissions.forEach { permission ->
+            if (ContextCompat.checkSelfPermission(
+                    context,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                Log.i(TAG, "Permission Needed: $permission")
+                permissionsToRequest.add(permission)
+            }
+        }
+        if (permissionsToRequest.isEmpty()) {
+            Log.i(TAG, "All required permissions are granted.")
+        } else {
+            Log.i(TAG, "All permissions are not granted yet.")
+
+            val shouldShowRationale = permissionsToRequest.any { permission ->
+                ActivityCompat.shouldShowRequestPermissionRationale(context, permission)
+            }
+            if (shouldShowRationale) {
+                Toast.makeText(context, "All permissions are not granted yet!", Toast.LENGTH_SHORT)
+                    .show()
+                showAlertDialogue(context, permissionsToRequest.toTypedArray(), launcher, message)
+            } else {
+                // permissions haven't been asked yet!
+                launcher.launch(permissionsToRequest.toTypedArray())
+            }
+        }
+    }
+
+    private fun showAlertDialogue(
+        context: Context,
+        permissions: Array<String>,
+        launcher: ActivityResultLauncher<Array<String>>,
+        message: String
+    ) {
+        AlertDialog.Builder(context).apply {
+            setTitle("Need permissions for smooth experience.")
+            setMessage(message)
+            setPositiveButton("Yes") { _,_ ->
+                launcher.launch(permissions)
+            }
+            setNegativeButton("No") { _,_ ->
+                Log.w(TAG, "Permissions $permissions denied.", )
+            }
+        }.create().show()
+    }
+}
