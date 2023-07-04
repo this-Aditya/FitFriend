2c11bed8bca0af6adacc6e6d6109fcc5fe87ba76
Aditya Mishra <adityamishra2217@gmail.com>
2023-07-05 01:04:54 +0530

    Added Classes for tracking sleep data

---
+++ app/src/main/java/com/aditya/fitfriend_android/models/DataLog.kt
@@ -0,0 +1,5 @@
+package com.aditya.fitfriend_android.models
+
+enum class DataLog(val numLogs: Int) {
+    TEN(10), CENT(100), MAX(-1)
+}
---
+++ app/src/main/java/com/aditya/fitfriend_android/models/SleepMetric.kt
@@ -0,0 +1,5 @@
+package com.aditya.fitfriend_android.models
+
+enum class SleepMetric {
+    SLEEP_SEGMENTS, SLEEP_CLASSIFY
+}
